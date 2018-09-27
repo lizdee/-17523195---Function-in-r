d673ab30bef71327481d91db67f57b3918d19437
lizdee <dimasbammer@gmail.com>
2018-09-27 17:23:24 +0700

    Test

---
+++ Function 101.r
@@ -0,0 +1,66 @@
+
+#rational function
+f <- function(x){
+    fx <- -5/x
+    return(fx)
+}
+
+
+input <- seq(1, 5, 0.1)
+plot (input,
+     sapply(input, f),
+     type="l",
+     xlab="x",
+     ylab="f(x)")
+
+#polynomial function
+f <- function(x){
+    fx <- (x^5-x)/x + x^2
+    return(fx)
+}
+
+input <- seq(-10,12,0.1)
+plot(input,
+    sapply(input,f),
+    type="l",
+    xlab="x",
+    ylab="f(x)")
+
+#quadratic function
+f <- function(x){
+    fx <- 5*x^2 - 3*x - 5
+    return(fx)
+}
+
+input <- -10:10
+plot(input,
+    sapply(input,f),
+    type="l",
+    xlab="x",
+    ylab="f(x)")
+
+#linear function
+f <- function(x){
+    fx <- 5*x-5
+    return(fx)
+}
+
+input <- -5:5
+plot(input,
+    sapply(input,f),
+    type="l",
+    xlab="x",
+    ylab="f(x)")
+
+#constant function
+f <- function(x){
+    fx <- 8
+    return(fx)
+}
+
+input <- 2:12
+plot(input,
+    sapply(input,f),
+    type="l",
+    xlab="x",
+    ylab="f(x)")
